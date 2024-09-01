747363133d64f8688b5df1c30b46209bb0303994
ethnvo <voethan2@gmail.com>
2024-08-31 18:35:42 -0700

    hid some things

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_API_KEY,
-    authDomain: "inventory-management-be758.firebaseapp.com",
-    projectId: "inventory-management-be758",
-    storageBucket: "inventory-management-be758.appspot.com",
-    messagingSenderId: "199171177978",
-    appId: "1:199171177978:web:b33715da18cdfe20e63843",
-    measurementId: "G-PNYTG355H3"
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
